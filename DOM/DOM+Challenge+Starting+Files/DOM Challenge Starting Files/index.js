e99c0c89bd3b55cb7a054279962e0f808d3cddb7
edBatiancila <batiancilaedmyr@gmail.com>
2025-02-28 16:52:10 +0800

    added new file

--- DOM/DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
+++ DOM/DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
@@ -1,1 +1,19 @@
-document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Angela";
+
+
+document.querySelector(".dark-mode").addEventListener("click", function() {
+    document.querySelector("body").classList.add("testing");
+    document.querySelector("body").style.color="white";
+    document.querySelector("h1").style.color="silver";
+});
+
+document.querySelector(".light-mode").addEventListener("click", function() {
+    document.querySelector("body").classList.remove("testing");
+    document.querySelector("body").style.color="black";
+    document.querySelector("h1").style.color="red";
+});
+
+document.querySelector(".checked").addEventListener("click", function() {
+    document.querySelector(".modes").classList.toggle("invisible");
+    document.querySelector("h1").classList.toggle("huge");
+});
+
