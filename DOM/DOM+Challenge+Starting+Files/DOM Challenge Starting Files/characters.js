c308504e8033e1ea449ae3197525cca8c6638a02
edBatiancila <batiancilaedmyr@gmail.com>
2025-03-02 00:01:58 +0800

    added new files

---
+++ DOM/DOM+Challenge+Starting+Files/DOM Challenge Starting Files/characters.js
@@ -0,0 +1,82 @@
+const characters = {
+    "btn-naruto": {
+        id: 1,
+        human: {
+            src: "images/naruto_human.gif",
+            name: "Human Naruto Uzumaki",
+            color: "white"
+        },
+        transformed: {
+            src: "images/naruto_kyuubi.gif",
+            name: "Kyuubi Naruto Uzumaki",
+            color: "orange"
+        }
+    },
+    "btn-kaneki": {
+        id: 2,
+        human: {
+            src: "images/kaneki_human.gif",
+            name: "Human Kaneki Ken",
+            color: "white"
+        },
+        transformed: {
+            src: "images/kaneki_ghoul.gif",
+            name: "Ghoul Kaneki Ken",
+            color: "red"
+        }
+    },
+    "btn-ichigo": {
+        id: 3,
+        human: {
+            src: "images/ichigo_human.gif",
+            name: "Human Ichigo Kurosaki",
+            color: "white"
+        },
+        transformed: {
+            src: "images/ichigo_hollow.gif",
+            name: "Hollow Ichigo Kurosaki",
+            color: "#FF4B33"
+        }
+    },
+    "btn-okarun": {
+        id: 4,
+        human: {
+            src: "images/okarun_human.gif",
+            name: "Human Ken Takakura",
+            color: "white"
+        },
+        transformed: {
+            src: "images/okarun_yokai.gif",
+            name: "Yokai Ken Takakura",
+            color: "#8B0000"
+        }
+    },
+    "btn-eren": {
+        id: 5,
+        human: {
+            src: "images/eren_human.gif",
+            name: "Human Eren Yeager",
+            color: "white"
+        },
+        transformed: {
+            src: "images/eren_titan.gif",
+            name: "Titan Eren Yeager",
+            color: "brown"
+        }
+    },
+    "btn-kafka": {
+        id: 6,
+        human: {
+            src: "images/kafka_human.gif",
+            name: "Human Kafka Hibino",
+            color: "white"
+        },
+        transformed: {
+            src: "images/kafka_kaiju.gif",
+            name: "Kaiju Kafka Hibino",
+            color: "#08FEFC"
+        }
+    }
+}
+
+export default characters;
--- DOM/DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
+++ DOM/DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
@@ -1,4 +1,4 @@
-
+import characters from "./characters.js";
 
 document.querySelector(".dark-mode").addEventListener("click", function() {
     document.querySelector("body").classList.add("testing");
@@ -17,3 +17,53 @@ document.querySelector(".checked").addEventListener("click", function() {
     document.querySelector("h1").classList.toggle("huge");
 });
 
+Object.keys(characters).forEach(buttonClass => {
+    document.querySelector(`.${buttonClass}`).addEventListener("click", function() {
+        const imgElement = document.querySelector(".pictures img");
+        const nameElement = document.querySelector(".character-name");
+        const buttonElement = document.querySelector(".transform");
+        const character = characters[buttonClass].human;
+
+        document.querySelectorAll(".menu-button button").forEach(btn => {
+            btn.classList.remove("character-selected");
+        });
+
+        this.classList.add("character-selected");
+
+        imgElement.setAttribute("src", character.src);
+        nameElement.textContent = character.name;
+        nameElement.style.color = character.color;
+        buttonElement.textContent = "Transform";
+    });
+});
+
+document.querySelector(".transform").addEventListener("click", function() {
+    const imgElement = document.querySelector(".pictures img");
+    const nameElement = document.querySelector(".character-name");
+    const buttonElement = document.querySelector(".transform");
+
+    const currentSrc = imgElement.getAttribute("src");
+    let transformed = false;
+
+    Object.keys(characters).forEach(buttonClass => {
+        const character = characters[buttonClass];
+
+        if(currentSrc === character.human.src) {
+            imgElement.setAttribute("src", character.transformed.src);
+            nameElement.textContent = character.transformed.name;
+            nameElement.style.color = character.transformed.color;
+            buttonElement.textContent = "Release";
+            transformed = true;
+        } else if(currentSrc === character.transformed.src) {
+            imgElement.setAttribute("src", character.human.src);
+            nameElement.textContent = character.human.name;
+            nameElement.style.color = character.human.color;
+            buttonElement.textContent = "Transform";
+            transformed = true;
+        }
+    });
+
+    if(!transformed) {
+        buttonElement.textContent = "Transform";
+    }
+});
