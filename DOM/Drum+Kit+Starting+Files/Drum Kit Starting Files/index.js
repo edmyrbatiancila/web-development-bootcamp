907daea8e52a194c88f47eafc9291f133fa0736b
edBatiancila <batiancilaedmyr@gmail.com>
2025-03-03 16:53:37 +0800

    added new file

--- DOM/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ DOM/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -5,56 +5,73 @@ for(let i = 0; i < numberOfdrums; i++) {
 
     drumElements[i].addEventListener("click", function() {
         const buttonTextContent = this.textContent;
-        const drumBorder = this.style.border = "2px solid yellow";
-        console.log(drumBorder);
         
-        switch (buttonTextContent) {
-            case "w":
-                const drumSoundTom1 = new Audio('sounds/tom-1.mp3');
-                drumSoundTom1.play();
-                drumBorder;
-                break;
-
-            case "a":
-                const drumSoundTom2 = new Audio('sounds/tom-2.mp3');
-                drumSoundTom2.play();
-                break;
-
-            case "s":
-                const drumSoundTom3 = new Audio('sounds/tom-3.mp3');
-                drumSoundTom3.play();
-                break;
-
-            case "d":
-                const drumSoundTom4 = new Audio('sounds/tom-4.mp3');
-                drumSoundTom4.play();
-                break;
-
-            case "j":
-                const drumSoundSnare = new Audio('sounds/snare.mp3');
-                drumSoundSnare.play();
-                break;
-
-            case "k":
-                const drumSoundCrash = new Audio('sounds/crash.mp3');
-                drumSoundCrash.play();
-                break;
-
-            case "l":
-                const drumSoundKick = new Audio('sounds/kick-bass.mp3');
-                drumSoundKick.play();
-                break;
-
-            default:
-                console.log(buttonTextContent);
-                break;
-        }
+        makeSound(buttonTextContent);
     });
 }
 
+document.addEventListener("keypress", function(event) {
+    const keyboard = event.key;
 
-// const drumSoundTom1 = new Audio('sounds/tom-1.mp3');
-//         drumSoundTom1.play();
+    makeSound(keyboard);
+});
 
+function makeSound(key) {
+    switch (key) {
+        case "r":
+            const drumSoundTom1 = new Audio('sounds/tom-1.mp3');
+            drumSoundTom1.play();
+            break;
+
+        case "u":
+            const drumSoundTom2 = new Audio('sounds/tom-2.mp3');
+            drumSoundTom2.play();
+            break;
+
+        case "i":
+            const drumSoundTom3 = new Audio('sounds/tom-3.mp3');
+            drumSoundTom3.play();
+            break;
+
+        case "t":
+            const drumSoundTom4 = new Audio('sounds/tom-4.mp3');
+            drumSoundTom4.play();
+            break;
+
+        case "f":
+            const drumSoundSnare = new Audio('sounds/snare.mp3');
+            drumSoundSnare.play();
+            break;
+
+        case "y":
+            const drumSoundCrash = new Audio('sounds/crash.mp3');
+            drumSoundCrash.play();
+            break;
+
+        case "v":
+            const drumSoundKick = new Audio('sounds/kick-bass.mp3');
+            drumSoundKick.play();
+            break;
+
+        case "h":
+            const drumSoundOpenHiHat = new Audio('sounds/open_hihat.mp3');
+            drumSoundOpenHiHat.play();
+            break;
+
+        case "j":
+            const drumSoundCloseHiHat = new Audio('sounds/close-hihat.mp3');
+            drumSoundCloseHiHat.play();
+            break;
+
+        case "n":
+            const drumSoundRide = new Audio('sounds/ride_cymbals.mp3');
+            drumSoundRide.play();
+            break;
+
+        default:
+            console.log(buttonTextContent);
+            break;
+    }
+}
 
 
--- DOM/script.js
+++ DOM/script.js
@@ -10,3 +10,25 @@ const houseKeeper1 = {
         "cooking"
     ]
 }
+
+// Example of Constructor Function:
+function BellBoy(name, age, hasWorkPermit, languages ) {
+    this.name - name;
+    this.age = age;
+    this.hasWorkPermit = hasWorkPermit;
+    this.languages = languages;
+}
+
+function HouseKeeper(yearsOfExperience, name, cleaningRepertoire, skills) {
+    this.yearsOfExperience = yearsOfExperience;
+    this.name = name;
+    this.cleaningRepertoire = cleaningRepertoire;
+    this.skills = skills;
+    this.clean = function() {
+        alert("Cleaning in progress...");
+    }
+}
+
+const houseKeeper2 = new HouseKeeper(10, "Bob", ["lobby", "kitchen", "chair"]);
+
+houseKeeper2.clean();
